46a748f4a4640537aca9e9a3f9de21c0bef47ef0
AmirrezaSaadi <amisogand0509@gmail.com>
2026-05-05 01:06:31 +0330

    Initial commit

---
+++ controllers/static.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/AmirrezaSaadi/lenslocked/views"
+)
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+
+	questions := []struct {
+		Question string
+		Answer   string
+	}{
+		{
+			Question: "Is it free?",
+			Answer:   "Yes it is fully free and Open-Source",
+		},
+
+		{
+			Question: "What is the meaning of life?",
+			Answer:   "42",
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/AmirrezaSaadi/lenslocked/controllers"
+	"github.com/AmirrezaSaadi/lenslocked/templates"
+	"github.com/AmirrezaSaadi/lenslocked/views"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	tpl := views.Must(views.ParseFS(templates.FS, "home.html", "tailwind.html"))
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.html", "tailwind.html"))
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.html", "tailwind.html"))
+	r.Get("/faq", controllers.FAQ(tpl))
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, ("404 " + http.StatusText(http.StatusNotFound)), http.StatusNotFound)
+	})
+
+	fmt.Println("Starting the server on :3000")
+	http.ListenAndServe(":3000", r)
+}
